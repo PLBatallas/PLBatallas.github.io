337f2dc9cc45c7f46ec34ebbc204c5efdad08162
PLBatallas <84447343+PLBatallas@users.noreply.github.com>
2021-07-20 14:40:52 +0200

    Update question.js

--- question.js
+++ question.js
@@ -7,16 +7,16 @@
 },
 */
 const quiz =[
-    {
-        q:'Wich month comes before june ?',
-        options:['may','september','july','august'],
+     {
+        q:'Wich of this sentences uses first conditional ?',
+        options:['If I study today I will go to the party tonight',
+        'If it rained, you would get wet',
+        'If you had told me about the meeting, I would have come'],
         answer:0
     },
     {
-       q:'Wich of this sentences uses first conditional ?',
-        options:['If I study today I will go to the party tonight',
-        'If it rained you would get wet',
-        'If you had told me about the meeting I would have come'],
+        q:'According to "Colors and teamwork", orange helps the team...',
+        options:['Get it started','Get it right','Get it completed','Stay together'],
         answer:0
     },
     {
@@ -25,14 +25,13 @@ const quiz =[
         answer:0
     },
     {
-        q:'What time of day do we have breakfast?',
-        options:['Afternoon','Evening','Morning'],
-        answer:2
+        q:'If a value does not change, we could say it',
+        options:['Increases','Remains steady','fluctuate'],
+        answer:1
     },
     {
-        q:'What is 22 + 6?',
+        q:'How much is 22 + 6?',
         options:['99','28','44','12'],
         answer:1
-    } //observe que la última pregunta no cierra con coma ,
-    
+    }//observe que la última pregunta no se cierra con ,
 ]
